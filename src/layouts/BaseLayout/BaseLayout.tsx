f36f43b02c3b27513a7d380bc8a4b3624c19b4e6
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2022-10-14 17:15:02 +0300

    added favicons

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -9,6 +9,33 @@ const BaseLayout: FC<Props> = ({ children }) => {
     <>
       <Head>
         <title>Metamoon</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <meta name="description" content="" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Content>
         <Inner>{children}</Inner>
