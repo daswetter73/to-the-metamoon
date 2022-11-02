71d5cf6888d282799ee260e6a705e76f4f8035b7
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2022-11-02 11:29:39 +0300

    added public sale content

---
+++ src/shared/components/PublicSaleContent/PublicSaleContent.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+const BalanceWrapper = styled.div`
+  max-width: 700px;
+`;
+const Title = styled.h1`
+  font-size: 40px;
+  margin: 0 0 30px 0;
+  font-weight: 500;
+`;
+const Description = styled.div`
+  font-size: 32px;
+  margin-bottom: 50px;
+`;
+const ButtonWrapper = styled.div`
+  margin: 40px 0;
+`;
+export { Title, Description, BalanceWrapper, ButtonWrapper };
---
+++ src/shared/components/PublicSaleContent/PublicSaleContent.tsx
@@ -0,0 +1,37 @@
+import BalanceItem from '../BalanceItem/BalanceItem';
+import Button from '../Button/Button';
+import {
+  BalanceWrapper,
+  ButtonWrapper,
+  Description,
+  Title,
+} from './PublicSaleContent.styled';
+
+const PublicSaleContent = () => {
+  const { id, title, amount } = { id: 0, title: 'Public Price', amount: '0' };
+
+  const handleSubscribeClick = () => {};
+  return (
+    <>
+      <Title>Subscribe to our news!</Title>
+      <Description>
+        We’ll send you a notification when public sale is available to
+        participate
+      </Description>
+      <BalanceWrapper>
+        <BalanceItem id={id} title={title} amount={amount} />
+      </BalanceWrapper>
+      <ButtonWrapper>
+        <Button
+          isUpperCase={true}
+          maxWidth="200px"
+          onClick={handleSubscribeClick}
+        >
+          subscribe
+        </Button>
+      </ButtonWrapper>
+    </>
+  );
+};
+
+export default PublicSaleContent;
